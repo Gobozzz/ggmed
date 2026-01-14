9a410f4e0568b54fa6e228860757616573f5dadb
Gobozzz <gobozovbogdan@gmail.com>
2026-01-14 15:25:15 +0300

    Создание и базовая настройка админ панели

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use MoonShine\Laravel\Models\MoonshineUser;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        MoonshineUser::factory()->create([
+            'name' => 'Admin GGMED',
+            'email' => 'admin-files@ggmed.ru',
+            'password' => Hash::make('ggmed_14&01&2026!'),
+            'avatar' => '/admins/admin-files.jpg'
         ]);
     }
 }
